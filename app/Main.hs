71fdfe5f5565f79e7fce356a0d309f14a9ff048a
conf8o <41929738+conf8o@users.noreply.github.com>
2024-01-04 23:47:56 +0900

    fix code style

--- app/Main.hs
+++ app/Main.hs
@@ -10,13 +10,10 @@ import Network.HTTP.Types (StdMethod (GET))
 import qualified Data.Text as T
 import qualified Data.ByteString as B
 
-main :: IO ()
-main = Warp.run 8000 router
-
 type Path = [T.Text]
-type Routinginfo = (HTypes.StdMethod, Path)
+type RoutingInfo = (HTypes.StdMethod, Path)
 
-toRoutingInfo :: Wai.Request -> Either B.ByteString Routinginfo
+toRoutingInfo :: Wai.Request -> Either B.ByteString RoutingInfo
 toRoutingInfo req =
     fmap (, Wai.pathInfo req)
     $ HTypes.parseMethod . Wai.requestMethod
@@ -28,19 +25,26 @@ router req =
         Right routingInfo -> routing routingInfo req
         _ -> notFound req
 
-routing :: Routinginfo -> Wai.Application
+routing :: RoutingInfo -> Wai.Application
 routing (GET, [""]) = getIndex
 routing (GET, ["articles"]) = getArticles
 routing _ = notFound
 
+main :: IO ()
+main = Warp.run 8000 router
+
 getIndex :: Wai.Application
-getIndex _ send
-    = send $ Wai.responseBuilder HTypes.status200 [] "OK"
+getIndex _ send =
+    send $ Wai.responseBuilder HTypes.status200 [] "OK"
 
 getArticles :: Wai.Application
-getArticles req send
-    = send $ Wai.responseBuilder HTypes.status200 [] "hello wai"
+getArticles req send = 
+    send $ Wai.responseBuilder HTypes.status201 [] ""
+
+postArticles :: Wai.Application
+postArticles req send =
+    send $ Wai.responseBuilder HTypes.status201 [] ""
 
 notFound :: Wai.Application
-notFound _ send
-  = send $ Wai.responseBuilder HTypes.status404 [] "not found"
+notFound _ send =
+    send $ Wai.responseBuilder HTypes.status404 [] "not found"
